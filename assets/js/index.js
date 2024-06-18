747cfd901fcb0043fa87efe0fa400bc960607d03
mirko-mc <8547740+mirko-mc@users.noreply.github.com>
2024-06-18 22:17:08 +0200

    - gli artisti vengono caricati dinamicamente nella aside saltando gli inesistenti sul server - album nella sezione collapse = adattare JS con HTML

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,96 @@
-const ENDPOINT = "https://striveschool-api.herokuapp.com/api/deezer/search?q=";
+const URLSEARCH = "https://striveschool-api.herokuapp.com/api/deezer/search?q=";
+let URLARTIST = "https://striveschool-api.herokuapp.com/api/deezer/artist/";
 let searchs = null;
+let artist = null;
+
+document.addEventListener("DOMContentLoaded", () => {
+  asideArtist();
+});
+
+async function getArtist(query) {
+  const RESPONSE = await fetch(URLARTIST + query);
+  if (RESPONSE.status === 500) return false;
+
+  //   await getArtist(Math.random() * 10);
+  // } else {
+  //   // console.log("GETARTIST => response\n", response);
+  // }
+  const data = await RESPONSE.json();
+  // console.log("GETARTIST => data\n", data);
+  artist = {
+    artistName: data["name"],
+    pictureSmall: data["picture_small"],
+    coverMedium: data["picture_medium"],
+    coverBig: data["picture_big"],
+    coverXl: data["picture_xl"],
+  };
+  // console.log("GETARTIST => artist\n", artist);
+  return true;
+}
+
+/** fetch per la ricerca dell'artista passato come parametro in ingresso
+ *  prendo i dati ricevuti e mappo in un array di oggetti solo quelli che mi servono
+ */
 async function getSearch(query) {
-  const response = await fetch(ENDPOINT + query);
+  const response = await fetch(URLSEARCH + query);
   if (!response.ok) {
-    throw new Error("Network response was not ok");
+    throw new Error("OOPS.....ERROR");
   } else {
-    console.log("GETSEARCH => response\n", response);
+    // console.log("GETSEARCH => response\n", response);
   }
   const data = await response.json();
-  console.log("GETSEARCH => data\n", data.data);
+  // console.log("GETSEARCH => data\n", data.data);
   searchs = data.data.map((ALBUM) => ({
     idAlbum: ALBUM.album.id,
-    cover: ALBUM.album.cover_small,
+    cover: ALBUM.album.cover,
+    coverSmall: ALBUM.album.cover_small,
+    coverMedium: ALBUM.album.cover_medium,
+    coverBig: ALBUM.album.cover_big,
+    coverXl: ALBUM.album.cover_xl,
     title: ALBUM.title_short,
     artist: ALBUM.artist.name,
     idArtist: ALBUM.artist.id,
     pictureArtist: ALBUM.artist.picture_small,
     duration: ALBUM.duration,
   }));
-  console.log("GETSEARCH => search\n", searchs);
+  // console.log("GETSEARCH => search\n", searchs);
 }
 
-async function leftAsideAlbum() {
-  await getSearch("queen");
-  const LEFTASIDE = document.getElementById("leftAside");
-  for (const SEARCH of searchs) {
-    LEFTASIDE.innerHTML += `
-            <img src="${SEARCH.cover}" alt="">
-            <h6>${SEARCH.title}</h6>
-            <p>${SEARCH.artist}</p>
-            `;
+/** aside => artist */
+async function asideArtist() {
+  artist = null;
+  for (let i = 0; i <= 5; i++) {
+    if ((await getArtist(Math.random() * 10)) === false) {
+      console.log("ASIDEARTIST => ho saltato l'elemento perché non esiste");
+      i--;
+      continue;
+    }
+    document.getElementById("recents-artists-aside-list").innerHTML += `
+    <li>
+      <a href="">
+      <img src="${artist.pictureSmall}" alt="${artist.artistName}">
+      <div>
+        <div>${artist.artistName}</div>
+        <div>Artist</div>
+      </div>
+      </a>
+    </li>
+    `;
   }
 }
+
+/** collapse => album */
+async function collapseAlbum() {
+  await getSearch("queen");
+  const RANDOM = parseInt(Math.random() * searchs.length);
+  const RIGHTASIDE = document.getElementById("rightAside");
+  RIGHTASIDE.innerHTML += `
+              <img src="${searchs[RANDOM].coverBig}" alt="">
+              <h6>${searchs[RANDOM].title}</h6>
+              <p>${searchs[RANDOM].artist}</p>
+              <span class="material-symbols-outlined">check_circle</span>
+              `;
+}
+
+/** collapse => informazioni sull'artista */
+async function collapseArtistInfo() {}
