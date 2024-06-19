2ae65545c5a49cc204cee55b22c6f770e79fca87
Chrisolivieri <christian.olivieri93@gmail.com>
2024-06-19 16:17:31 +0200

    search params

--- assets/js/album.js
+++ assets/js/album.js
@@ -1,25 +1,27 @@
 
 const api = "https://striveschool-api.herokuapp.com/api/deezer/album/"
 let album = null
+const params = new URLSearchParams(window.location.search)
 
-async function getAlbum(id) {
-    const response = await fetch(api + id)
+console.log(params)
+
+const id = params.get("id")
+const title = params.get("title")
+const cover = params.get("cover")
+const cover_xl = params.get("cover_xl")
+const tracks = params.get("tracks")
+const release_date = params.get("release_date")
+const name = params.get("name")
+
+getAlbum()
+
+async function getAlbum() {
+    const response = await fetch(api)
     const data = await response.json()
     console.log(data)
 
-    album = {
-        id: data.id,
-        title: data.title,
-        cover: data.cover,
-        coverXl: data.cover_xl,
-        coverSmall: data.cover_small,
-        tracks: data.tracks.data,
-        releaseDate: data.release_date,
-        name: data.artist.name
-    }
-    
     let songs = ""
-    album.tracks.map((track, i) => {
+    tracks.map((track, i) => {
         songs += `<tr>
                     <th scope="row">${i + 1}</th>
                     <td>${track.title}</td>
@@ -38,15 +40,15 @@ async function getAlbum(id) {
     album = `<div class="card mb-3" style="width:100%;">
   <div class="row g-0">
     <div class="col-md-4">
-      <img src=${album.coverXl} class="img-fluid rounded-start" alt="...">
+      <img src=${cover_xl} class="img-fluid rounded-start" alt="...">
     </div>
     <div class="col-md-8">
       <div class="card-body">
       <p class="card-text"><small class="text-body-secondary">Album</small></p>
-        <h1 class="card-title">${album.title}</h1>
-        <span class="card-text"><small class="text-body-secondary">${album.name}</small></span>
-        <span class="card-text"><small class="text-body-secondary">• ${album.releaseDate}</small></span>
-        <span class="card-text"><small class="text-body-secondary">• ${album.tracks.length} tracks</small></span>
+        <h1 class="card-title">${title}</h1>
+        <span class="card-text"><small class="text-body-secondary">${name}</small></span>
+        <span class="card-text"><small class="text-body-secondary">• ${release_date}</small></span>
+        <span class="card-text"><small class="text-body-secondary">• ${tracks.length} tracks</small></span>
         
       </div>
     </div>
