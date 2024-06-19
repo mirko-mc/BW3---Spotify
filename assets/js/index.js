4d6ffe82685ff0f16df2f689e832b2b1c8ae8c4e
mirko-mc <8547740+mirko-mc@users.noreply.github.com>
2024-06-19 21:21:03 +0200

    index.js - show cards in main

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,39 +7,24 @@ let album = null;
 
 document.addEventListener("DOMContentLoaded", () => {
   asideArtist();
-  // albumCards();
 });
 
 /** aside => artist */
 async function asideArtist() {
-
   await getSearch("a");
+  albumCards();
   console.log("ASIDEARTIST => album");
   console.table(album);
   console.log("ASIDEARTIST => artist");
   console.table(artist);
-for (let i = 0; i < 15; i++) {
-  const ALBUM = album[i];
-  const ARTIST = artist[i];
-
-  // let random = null;
-  // const RANDOMHISTORY = [];
-  // for (let i = 0; i < 13; i++) {
-  //   do {
-  //     random = parseInt(Math.random() * 300);
-  //   } while (RANDOMHISTORY.includes(random));
-  //   RANDOMHISTORY.push(random);
-  //   if ((await getArtist(random)) === false) {
-  //     console.log("ASIDEARTIST => ho saltato l'elemento perché non esiste");
-  //     i--;
-  //     continue;
-  //   }
+  for (let i = 0; i < 16; i++) {
+    const ARTIST = artist[i];
     document.getElementById("recent-artists-aside-list").innerHTML += `
     <li>
-      <a href="./artist.html?id=${artist.id}">
-      <img src="${artist.pictureSmall}" alt="${artist.artistName}">
+      <a href="./artist.html?id=${ARTIST.id}">
+      <img src="${ARTIST.pictureSmall}" alt="${ARTIST.name}">
       <div>
-        <div>${artist.artistName}</div>
+        <div>${ARTIST.name}</div>
         <div>Artist</div>
       </div>
       </a>
@@ -65,33 +50,26 @@ async function collapseAlbum() {
 async function collapseArtistInfo() {}
 
 async function albumCards() {
-  // album = null;
-  let random = null;
-  const RANDOMHISTORY = [];
-  for (let i = 0; i < 5; i++) {
-    do {
-      random = parseInt(Math.random() * 300);
-    } while (RANDOMHISTORY.includes(random));
-    RANDOMHISTORY.push(random);
-    console.log(random);
-    if ((await getAlbum(random)) === false) {
-      console.log("ALBUMCARDS => ho saltato l'elemento perché non esiste");
-      i--;
-      continue;
-    }
-    document.getElementById("cardsContainer").innerHTML += `
+  let y = 0;
+  for (let x = 0; x < 4; x++) {
+    const CARDSCONTAINER = document.getElementById(`cardsContainer${x + 1}`);
+    for (y; y < 16; y++) {
+      const ALBUM = album[y];
+      const ARTIST = artist[y];
+      CARDSCONTAINER.innerHTML += `
   <div class="card"">
     <div class="card-img-container">
-			<a href="./album.html/${album.idAlbum}"><img src="${album.coverMedium}" class="card-img-top" alt="ALBUM IMG"></a>
+			<a href="./album.html/${ALBUM.id}"><img src="${ALBUM.coverMedium}" class="card-img-top" alt="ALBUM IMG"></a>
 		</div>
 		<div class="card-body">
-			<a href="./album.html/${album.idAlbum}">
-			  <h5 class="card-title">${album.title}</h5>
+			<a href="./album.html?id=${ALBUM.id}">
+			  <h5 class="card-title">${ALBUM.title}</h5>
 			</a>
-			<a href="./album.html/${album.idArtist}">
-			  <p class="card-text">${album.artistName}</p>
+			<a href="./album.html?id=${ARTIST.id}">
+			  <p class="card-text">${ARTIST.name}</p>
 			</a>
 		</div>
 `;
+    }
   }
 }
