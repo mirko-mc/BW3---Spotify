acfe9439722e76bb075a25c7121f8224997b2a52
mirko-mc <8547740+mirko-mc@users.noreply.github.com>
2024-06-20 21:07:27 +0200

    delete unused file

--- assets/js/album.js
+++
@@ -1,9 +0,0 @@
-
-const api = "https://striveschool-api.herokuapp.com/api/deezer/album/"
-let album = null
-const params = new URLSearchParams(window.location.search)
-
-document.addEventListener("DOMContentLoaded", async () => {
-  await asideArtist();
-  await albumClick();
-})
